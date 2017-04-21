f78e92cfaf1fa0012bd2bdfdfed4c6d7c779cc18
Terry Chun <docterry@hotmail.com>
2017-04-20 17:22:56 -0700

    Get geolocation info from IP address

--- index.php
+++ index.php
@@ -39,8 +39,8 @@
     }
     $browser = $_SERVER['HTTP_USER_AGENT'];
     $phone = preg_match('/(iPhone|Android|Windows Phone)/i',$browser);
+    $geo = json_decode(file_get_contents('http://ipinfo.io/'.$_SERVER['REMOTE_ADDR']));
     $xml = simplexml_load_file("../paging/list.xml");
-    //$groups = ($xml->groups) ?: $xml->addChild('groups');
     $chip = simplexml_load_file('../patlist/currlist.xml');
     $call = array(
         'CICU',
