661a16c0f6f6575df2c5c8991ecc5c9f7832f36a
Terry Chun <docterry@hotmail.com>
2017-05-05 21:44:04 -0700

    Log the date, IP, and number clicked

--- result.php
+++ result.php
@@ -3,4 +3,35 @@
  * Perform this when onclick() button
  */
 $in = \filter_input(\INPUT_GET,'in');
-file_put_contents('timer.txt', $in."\r\n", FILE_APPEND);
+$logfile = 'logs/'.date('Ym').'.csv';
+$ipaddress = '';
+if (getenv('HTTP_CLIENT_IP')) {
+    $ipaddress = getenv('HTTP_CLIENT_IP');
+} else if(getenv('HTTP_X_FORWARDED_FOR')) {
+    $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+} else if(getenv('HTTP_X_FORWARDED')) {
+    $ipaddress = getenv('HTTP_X_FORWARDED');
+} else if(getenv('HTTP_FORWARDED_FOR')) {
+    $ipaddress = getenv('HTTP_FORWARDED_FOR');
+} else if(getenv('HTTP_FORWARDED')) {
+   $ipaddress = getenv('HTTP_FORWARDED');
+} else if(getenv('REMOTE_ADDR')) {
+    $ipaddress = getenv('REMOTE_ADDR');
+} else {
+    $ipaddress = 'UNKNOWN';
+}
+function logger($str) {
+    global $logfile, $ipaddress;
+    $out = fopen($logfile,'a');
+    fputcsv(
+        $out, 
+        array(
+            date('c'),
+            $ipaddress,
+            $str
+        )
+    ); 
+    fclose($out);
+}
+
+logger("tel:".$in);
